76a54d97814026a285738b21a2724331b61eff7a
Leonardo Lopes <leo2006lopes@gmail.com>
2024-01-26 11:46:34 -0300

    Chess layer

---
+++ src/bordgame/Piece.java
@@ -0,0 +1,14 @@
+package bordgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
